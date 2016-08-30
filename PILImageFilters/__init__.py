eabdc92bcaa5075294e8166f6babe52b24bc75ec
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-30 16:17:40 +0100

    [Feature] extend convert method for BaseFilter

--- PILImageFilters/__init__.py
+++ PILImageFilters/__init__.py
@@ -2,3 +2,7 @@ from BoundedValue import HexBoundedValue
 from Sepia import Sepia
 from OilPainting import OilPainting
 from Comic import Comic
+from Mosaic import Mosaic
+from Pencil import Pencil
+from Solarize import Solarize
+from Molten import Molten
